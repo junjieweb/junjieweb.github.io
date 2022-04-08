88d00b9aed1464c518d771d51fabc23f38ac0e5a
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-04-08 14:47:01 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import type {DefaultThemeOptions} from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
     // 站点配置
+    base: '/',
     lang: 'zh-CN',
     title: 'Junjie',
     description: 'Front-end developer',
