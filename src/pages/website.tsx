2ccba58c018d4ff4c66ff26170b3525d78ec5508
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-05-22 00:40:10 +0800

    Refactoring: Redesigning the navbar and sidebar

--- src/pages/website.tsx
+++ src/pages/website.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.css'
 
 export default function MyReactPage() {
     return (
-        <Layout>
+        <Layout title={`网站导航`}>
 
             <section className={styles.container}>
                 <h1 style={{textAlign: 'center'}}>网站导航</h1>
